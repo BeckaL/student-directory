ea16a513b4b9c2b06a359f10c21b1ad213a4b815
Becka <rebdok@hotmail.com>
2018-09-23 18:45:59 +0100

    refactored code and amended so that all students are counted when count is shown on input, including students previously input

--- directory.rb
+++ directory.rb
@@ -1,8 +1,10 @@
-def input_students
+@students = []
+
+def input_students(existing_students)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
+  students = existing_students.clone
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} student" + (students.count > 1? "s" : "")
@@ -16,25 +18,54 @@ def print_header
   puts "------------".center(60)
 end
 
-def print_names(students)
-  students.each.with_index do |student, i|
+def print_names
+  @students.each.with_index do |student, i|
     puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great student" + (@students.count > 1? "s" : "")
 end
 
-def run_directory
-  students = input_students
-  if students.empty?
-    puts "There are no students currently enrolled"
-  else
-    print_header
-    print_names(students)
-    print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_names
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      if @students.empty?
+        @students = input_students([])
+      else
+        @students = input_students(@students)
+      end
+    when "2"
+      if @students.empty?
+        puts "There are currently no students"
+      else
+        show_students
+      end
+    when "9" then exit
+    else
+      puts "I don't know what you meant, try again"
   end
 end
 
-run_directory
+def interactive_menu
+  loop do
+    print_menu
+    selection = gets.chomp
+    process(selection)
+  end
+end
+
+interactive_menu
