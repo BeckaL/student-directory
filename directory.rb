5c0a3dd257c320d696a9182992d2948e2d95089d
Becka <rebdok@hotmail.com>
2018-09-23 11:47:33 +0100

    Dealt with case where no students are added, dealt with pluralising of 'students' in string

--- directory.rb
+++ directory.rb
@@ -1,30 +1,40 @@
-students = [
-  {name: "Dr Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-            ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student" + (students.count > 1? "s" : "")
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "------------"
+  puts "The students of Villains Academy".center(60)
+  puts "------------".center(60)
 end
 
 def print_names(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+  students.each.with_index do |student, i|
+    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
+  end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-print_header
-print_names(students)
-print_footer(students)
+def run_directory
+  students = input_students
+  if students.empty?
+    puts "There are no students currently enrolled"
+  else
+    print_header
+    print_names(students)
+    print_footer(students)
+  end
+end
+
+run_directory
