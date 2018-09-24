b29b8c7e92df13e8ff171bf3f603781d5f128551
Becka <rebdok@hotmail.com>
2018-09-24 20:19:05 +0100

    added fuctionality to load data from csv and take argument (name of file to load) from command line

--- directory.rb
+++ directory.rb
@@ -3,12 +3,12 @@
 def input_students(existing_students)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   students = existing_students.clone
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} student" + (students.count > 1? "s" : "")
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   students
 end
@@ -72,8 +72,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -81,11 +81,23 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
 def interactive_menu
   load_students
   loop do
     print_menu
-    selection = gets.chomp
+    selection = STDIN.gets.chomp
     process(selection)
   end
 end
